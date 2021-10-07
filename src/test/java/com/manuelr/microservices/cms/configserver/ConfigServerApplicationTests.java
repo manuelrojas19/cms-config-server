5f7ec292f4e56b49a3c3b717db50797ea6fb4e49
Manuel <manuelarr99@gmail.com>
2021-10-07 18:56:55 -0500

    Configuration for microservices added

---
+++ src/test/java/com/manuelr/microservices/cms/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelr.microservices.cms.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
